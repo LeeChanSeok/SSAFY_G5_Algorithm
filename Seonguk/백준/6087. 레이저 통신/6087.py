7286ad1b6df995f855c1f8de96d4795054954059
kimtjddnr <38129927+kimtjddnr@users.noreply.github.com>
2022-08-11 02:34:21 +0900

    [BOJ] 6087. 레이저 통신 / 골드3

--- Seonguk/백준/6087. 레이저 통신/6087.py
+++ Seonguk/백준/6087. 레이저 통신/6087.py
@@ -16,7 +16,7 @@ sx,sy=pos[0]
 ex,ey=pos[1]
 dx=[-1,0,1,0]
 dy=[0,1,0,-1]
-#초기에 모든 칸에 거울 갯수를 inf로 초기화
+#초기에 모든 칸에 거울수를 inf로 초기화
 mirrorNum=[[float('inf')]*M for _ in range(N)]
 
 #출발점의 동서남북을 살피고 탐색 가능한 위치 정보 저장
@@ -24,17 +24,17 @@ q=deque()
 for i in range(4):
     nx, ny=sx+dx[i], sy+dy[i]
     if 0<=nx<N and 0<=ny<M and arr[nx][ny] == '.':
-        q.append((nx,ny,i,0))   #x, y, 방향, 거울 개수
+        q.append((nx,ny,i,0))   #x, y, 방향, 거울수
 
 while q:
     x,y,dir,cnt=q.popleft()
     for i in range(4):
         nx, ny=x+dx[i], y+dy[i]
-        ncnt=cnt+1 if dir != i else cnt #방향이 다르면 거울개 수 + 1
+        ncnt=cnt+1 if dir != i else cnt #방향이 다르면 거울수 + 1
 
-        #재방문하는 경우, nx,ny의 거울 개수보다 작거나 같은 경우에만 방문
+        #재방문하는 경우, nx,ny의 거울수보다 작거나 같은 경우에만 방문
         if 0<=nx<N and 0<=ny<M and arr[nx][ny] == '.' and mirrorNum[nx][ny] >= ncnt:
-            mirrorNum[nx][ny]=ncnt  #더 적은 개수로 초기화
+            mirrorNum[nx][ny]=ncnt  #더 적은 거울수로 초기화
             q.append((nx,ny,i,ncnt))
 
 print(mirrorNum[ex][ey])
