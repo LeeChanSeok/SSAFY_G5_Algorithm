5b55bdc1f301cacaf935f6204ee8e2d9a8a6e1c3
GunHee.Lee <58353358+o-o-wine@users.noreply.github.com>
2022-08-15 20:14:27 +0900

    Update 1963.java

--- LeeGunHee/1963. 소수경로/1963.java
+++ LeeGunHee/1963. 소수경로/1963.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Scanner;
+
 import java.util.Queue;
 import java.util.StringTokenizer;
  
@@ -14,7 +14,6 @@ public class Main {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        Scanner sc = new Scanner(System.in);
         n = Integer.parseInt(br.readLine());
  
         prime[0] = prime[1] = true;
