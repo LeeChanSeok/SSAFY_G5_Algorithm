997521da0bd5837f752aee983b0f66f916793af6
GunHee.Lee <58353358+o-o-wine@users.noreply.github.com>
2022-08-15 20:02:01 +0900

    Update 1963.java

--- LeeGunHee/1963. 소수경로/1963.java
+++ LeeGunHee/1963. 소수경로/1963.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.Scanner;
 import java.util.Queue;
 import java.util.StringTokenizer;
  
