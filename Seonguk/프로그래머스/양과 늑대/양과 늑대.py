2d27389a3917b775d811b26bd8dabda912678ef2
kimtjddnr <38129927+kimtjddnr@users.noreply.github.com>
2022-08-22 19:11:35 +0900

    [PGS] 양과 늑대 / Lv3

---
+++ Seonguk/프로그래머스/양과 늑대/양과 늑대.py
@@ -0,0 +1,32 @@
+answer = 0
+
+def dfs(info, tree, idx, sheep, wolf, visitable):
+    global answer
+
+    if info[idx] == 0:
+        sheep+=1
+    else:
+        wolf+=1
+        
+    if wolf >= sheep:
+        return
+    
+    answer=max(answer, sheep)
+    
+    cur_visitable=visitable[:]  # 리스트 복사
+    cur_visitable.remove(idx)   # 현재 노드 삭제
+    cur_visitable.extend(tree[idx]) # 자식 노드들 추가
+
+    for v in cur_visitable:
+        dfs(info, tree, v, sheep, wolf, cur_visitable)
+
+def solution(info, edges):
+    global answer
+    tree={i:[] for i in range(len(info))}
+    
+    for p, c in edges:
+        tree[p].append(c)
+    
+    dfs(info, tree, 0, 0, 0, [0])
+    
+    return answer
