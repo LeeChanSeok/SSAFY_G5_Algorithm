5b8c23c748be2cf014391adcd8d58baa0a47e218
Seonguk Kim <38129927+kimtjddnr@users.noreply.github.com>
2022-10-03 19:18:13 +0900

    [BOJ] 23290. 마법사 상어와 복제 / G1

--- Seonguk/백준/23290. 마법사 상어와 복제/23290.py
+++ Seonguk/백준/23290. 마법사 상어와 복제/23290.py
@@ -1,4 +1,6 @@
 #마법사 상어와 복제
+#실행시간 : 4492ms(python3)
+
 import sys
 from collections import deque
 from itertools import product
@@ -99,3 +101,100 @@ for x in range(4):
         answer+=len(arr[x][y])
 
 print(answer)
+
+
+###########################################################################################################
+https://www.acmicpc.net/source/49748165
+# 실행시간 : 76ms(python3)
+    
+import sys
+input = sys.stdin.readline
+
+M, S = map(int, input().split())
+
+f_board = [[[0]*8 for _ in range(4)] for __ in range(4)]
+
+def _int(n):
+    return int(n)-1
+
+for _ in range(M):
+    fx, fy, fd = map(_int, input().split())
+    f_board[fx][fy][fd] += 1
+
+sx, sy = map(_int, input().split())
+
+dx = (0, -1, -1, -1, 0, 1, 1, 1) # @@ 9번째 항목은 제자리 방향
+dy = (-1, -1, 0, 1, 1, 1, 0, -1)
+
+sdx = (-1, 0, 1, 0)    # 상좌하우 순
+sdy = (0, -1, 0, 1)
+
+s_board = [[-3]*4 for _ in range(4)]
+def move_fish():
+    rnd = round-2 ####
+    tmp_board = [[[0]*8 for _ in range(4)] for __ in range(4)]
+    for x in range(4):
+        for y in range(4):
+            for d in range(8):
+                if not f_board[x][y][d]:
+                    continue
+                dir = d
+                for dm in range(8):
+                    nd = (dir-dm) % 8
+                    nx, ny = x+dx[nd], y+dy[nd]
+                    if nx < 0 or nx >= 4 or ny < 0 or ny >= 4 or (s_board[nx][ny] >= rnd) or (nx==sx and ny==sy):
+                        continue
+                    tmp_board[nx][ny][nd] += f_board[x][y][d]
+                    break
+                else:
+                    tmp_board[x][y][d] += f_board[x][y][d]
+    return tmp_board
+
+def move_shark():
+    global sx, sy
+    max_path = list()
+    max_cnt = -1
+    check = [[0]*4 for _ in range(4)]
+    def recur(x, y, path, cnt):
+        nonlocal max_cnt, max_path
+        if len(path) == 3:
+            if max_cnt < cnt:
+                max_cnt = cnt
+                max_path = [x for x in path]
+            return
+
+        for i in range(4):
+            nx, ny = x+sdx[i], y+sdy[i]
+            if nx < 0 or nx >= 4 or ny < 0 or ny >= 4:
+                continue
+            add = sum(f_board[nx][ny]) if not check[nx][ny] else 0  # @@ 왔다리 갔다리하면서 중복으로 선택하면 안되기때문에 check을 확인해야해
+            check[nx][ny] += 1
+            recur(nx, ny, path+[i], cnt+add)
+            check[nx][ny] -= 1   # @@ 다시 후진하면서 내 흔적을 지워버림
+
+    recur(sx, sy, [], 0)
+
+    for d in max_path:
+        nx, ny = sx+sdx[d], sy+sdy[d]
+        if sum(f_board[nx][ny]): s_board[nx][ny] = round     # @@ 여기서 물고기 안먹은경우 냄새 남기면 안돼
+        f_board[nx][ny] = [0] * 8   # 물고기 지우기
+        sx, sy = nx, ny
+
+
+if __name__ == "__main__":
+    for round in range(S):
+        copy_board = [[col[:] for col in row] for row in f_board]
+        f_board = move_fish()
+        move_shark()
+        # 복제 마법
+        for x in range(4):
+            for y in range(4):
+                for d in range(8):
+                    f_board[x][y][d] += copy_board[x][y][d]
+    
+    answer = 0
+    for x in range(4):
+        for y in range(4):
+            answer += sum(f_board[x][y])
+    
+    print(answer)
